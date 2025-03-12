a6096336ce74b2bc5216b0b7bc6f1b2c33763e82
Patrick Kerwood <patrick@kerwood.dk>
2025-03-12 10:15:46 +0100

    updated nodejs

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-const express = require('express')
-const morgan = require('morgan')
-const app = express()
-app.use(morgan('combined'))
+import express from "express";
+import morgan from "morgan";
+import process from "node:process";
+const app = express();
+app.use(morgan("combined"));
 
-const message = process.env.MESSAGE || 'Hello World'
+const message = process.env.MESSAGE || "Hello World";
 
-app.get('/', function (req, res) {
-  res.send(`<br><br><center><pre><h1>${message}</h1></pre></center>`)
-})
+app.get("/", function (_req, res) {
+  res.send(`${message}\n`);
+});
 
-console.log('Hello World application has started on port 3000')
-console.log('Powered by a bunch of small penguins...')
-app.listen(3000)
+console.log("Hello World application has started on port 3000");
+console.log("Powered by a bunch of small penguins...");
+app.listen(3000);
